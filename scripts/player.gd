d6e7119ae424ac2d8c509b005b3e9ea72e16a736
Nat10k <156871636+NoArt278@users.noreply.github.com>
2025-02-01 12:19:17 +0700

    feat : art

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,7 @@ class_name Player
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var currInteractable : Interactable
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 func _physics_process(_delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
@@ -18,6 +19,11 @@ func _physics_process(_delta: float) -> void:
 		velocity.y = y_dir * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	if velocity.x < 0 : 
+		sprite_2d.flip_h = true
+	elif velocity.x > 0 :
+		sprite_2d.flip_h = false
 	
 	if Input.is_action_just_pressed("interact") and currInteractable :
 		currInteractable.interact()
